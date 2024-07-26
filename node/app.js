e67bc73c8db7a9c7887a77d46741e6fd29075522
Sanskar Suri <sanskarsuri50@gmail.com>
2024-07-26 21:40:23 +0530

    fixed the css add css files in public/css

--- node/app.js
+++ node/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/welcome');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 
 app.use(userRoutes);
 
